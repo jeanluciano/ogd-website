162907b05f7101ab108f31067b7b43ab63e3892a
jeanluciano <jeanluciano@mailbox.org>
2021-02-04 23:32:00 -0600

    env

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: "https://ogd-backend.herokuapp.com",
         contentTypes: [
           "images",
           "tags",
